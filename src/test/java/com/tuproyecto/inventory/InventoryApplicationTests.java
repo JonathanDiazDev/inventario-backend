62f325bcb2609b4691730de6030a8382b70cb815
jonathandiaz.dev@gmail.com <jonathandiaz.dev@gmail.com>
2026-02-06 13:22:27 -0600

    fix: force H2 database for tests

--- src/test/java/com/tuproyecto/inventory/InventoryApplicationTests.java
+++ src/test/java/com/tuproyecto/inventory/InventoryApplicationTests.java
@@ -2,8 +2,11 @@ package com.tuproyecto.inventory;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 
 @SpringBootTest
+// 👇 CAMBIA ESTO
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 class InventoryApplicationTests {
 
 	@Test
